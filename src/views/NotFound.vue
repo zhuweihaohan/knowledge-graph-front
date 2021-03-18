a4dcc3c0e0c75852d05cea60c4225a04c15b867b
luohongde <1171194016@qq.com>
2021-03-18 14:58:53 +0800

    删除login页，增加404页

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="background-color: #4c4c4c">
+    <div style="text-align: center;color: #ff7300;"><h1>404，页面找不到了，请核对网址</h1></div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound"
+    }
+</script>
+
+<style scoped>
+
+</style>
